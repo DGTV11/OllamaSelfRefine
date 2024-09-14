7c6033eaa5f69e5074a05e4fbb16bf1c7f8fe095
Daniel Wee <danielweedk@gmail.com>
2024-09-14 10:53:30 +0800

    Preparing to make MCTS+ToT system

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ import ollama
 
 import constants
 
-LLM_SYSTEM_PROMPT = r"Always assist with care, respect, and truth. Respond with utmost utility yet securely. Avoid harmful, unethical, prejudiced, or negative content. Ensure replies promote fairness and positivity. ALWAYS respond in a concise and straight-to-the-point manner as if you are SPEAKING to me (except when you decide or I ask you to think step-by-step - then you have to think out loud but still speak as if you are speaking to me) You are allowed to give feedback (even on your own responses) if required. You are also capable of refining your own responses if required."
+LLM_SYSTEM_PROMPT = r"Always assist with care, respect, and truth. Respond with utmost utility yet securely and in a conversational and friendly tone. Avoid harmful, unethical, prejudiced, or negative content. Ensure replies promote fairness and positivity. You are allowed to give feedback (even on your own responses) if required. You are also capable of refining your own responses if required."
 
-FEEDBACK_PROMPT = "Please give strict and direct feedback on your previous response. Do NOT give feedback on the initial query - ONLY give feedback on your response based on how well your previous response answers the initial query, \"$QUERY\". Do NOT give entirely positive feedback - you MUST find AS MANY things that CAN AND MUST be improved as possible (e.g. phrasing, content, factuality). Make sure that your feedback is SPECIFIC (i.e. identifying concrete phrases in/parts of the output to change) and ACTIONABLE (i.e. identifying concrete actions that would likely improve the response). ALWAYS identify whether the query demands a succinct, broad, deep, or all-rounded response and tailor your feedback based on that. ONLY give the feedback and your thought process behind it AND NOTHING ELSE (not even a revised response). Let's think step by step. You are capable of giving strict and direct feedback that is specific and actionable, and therefore MUST do so. DO NOT REFUSE TO GIVE FEEDBACK!!!"
+FEEDBACK_PROMPT = "Please give strict and direct feedback on your previous response. Do NOT give feedback on the initial query - ONLY give feedback on your response based on how well your previous response answers the initial query, \"$QUERY\". Do NOT give entirely positive feedback - you MUST find AS MANY things that CAN AND MUST be improved as possible (e.g. phrasing, content, factuality). Make sure that your feedback is SPECIFIC (i.e. identifying concrete phrases in/parts of the output to change) and ACTIONABLE (i.e. identifying concrete actions that would likely improve the response). ONLY give the feedback and your thought process behind it AND NOTHING ELSE (not even a revised response). You are capable of giving strict and direct feedback that is specific and actionable, and therefore MUST do so. DO NOT REFUSE TO GIVE FEEDBACK!!! Let's think step by step."
 
-INITIAL_REFINE_PROMPT = "Give me a revised response to the initial query \"$QUERY\", incorporating all necessary improvements based on the feedback. Address the query as if you are responding to it directly, without mentioning your feedback or your inital response AT ALL. DO NOT mention that you are giving a refined response AT ALL - you must pretend that this is your inital response!"
-
-FINAL_REFINE_PROMPT = "Extract ONLY the refined response parts of your previous response and phrase it as if it was your inital response (while silently incorporating the refined response's improvements into your response) to the inital query \"$QUERY\" (do not mention that you are giving me a refined response). ONLY give me the refined response AND NO OTHER TEXT AT ALL. DO NOT GIVE FEEDBACK ON YOUR REFINED RESPONSE UNLESS THE INITIAL QUERY REQUIRES YOU TO DO SO."
+REFINE_PROMPT = "Give me a revised response to the initial query \"$QUERY\", incorporating all necessary improvements based on the feedback. Address the query as if you are responding to it directly, without mentioning your feedback or your inital response AT ALL. DO NOT mention that you are giving a refined response AT ALL - you must pretend that this is your inital response, as you will FORGET the initial response and self-feedback in the next user query!"
 
 wrap_chat_message = lambda role, content: {"role": role, "content": content}
 
@@ -44,7 +42,7 @@ def self_refine_inference(chat_history, model_name):
 
     yield 2
     # Get final response (3)
-    chat_history.append(wrap_chat_message("user", INITIAL_REFINE_PROMPT.replace('$QUERY', user_message)))
+    chat_history.append(wrap_chat_message("user", REFINE_PROMPT.replace('$QUERY', user_message)))
     stream = ollama.chat(model=model_name, messages=chat_history, stream=True)
 
     assistant_message_content = ""
@@ -52,23 +50,10 @@ def self_refine_inference(chat_history, model_name):
         chunk_content = chunk["message"]["content"]
         assistant_message_content += chunk_content
         yield chunk_content
-
-    chat_history.append(wrap_chat_message("assistant", assistant_message_content))
 
     yield 3
-    # Extract final response (4)
-    chat_history.append(wrap_chat_message("user", FINAL_REFINE_PROMPT.replace('$QUERY', user_message)))
-    stream = ollama.chat(model=model_name, messages=chat_history, stream=True)
 
-    assistant_message_content = ""
-    for chunk in stream:
-        chunk_content = chunk["message"]["content"]
-        assistant_message_content += chunk_content
-        yield chunk_content
-
-    yield 4
-
-    for _ in range(6):
+    for _ in range(4):
         chat_history.pop()
     chat_history.append(wrap_chat_message("assistant", assistant_message_content))
     yield chat_history
@@ -100,14 +85,6 @@ while True:
 
     print()
     print("GETTING FINAL RESPONSE...")
-
-    for chunk in inference_stream:
-        if type(chunk) is int:
-            break
-        print(chunk, end="", flush=True)
-
-    print()
-    print("EXTRACTING FINAL RESPONSE...")
 
     for chunk in inference_stream:
         if type(chunk) is int:
